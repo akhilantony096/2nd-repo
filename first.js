5e30a6d2d6628cc9de6587a4ea1f57d14167bd55
akhil <akhilpa44@gamil.com>
2021-03-31 09:52:39 +0530

    1st commit

---
+++ first.js
@@ -0,0 +1,6 @@
+var name1=("akhil")
+var age=24
+//console.log("my name is "+name1+" and my age is "+age) 
+var company=("luminar")
+var place="kakkanad"
+console.log(`our company is${company} it is loacted in ${place}`)
---
+++ swap.js
@@ -0,0 +1,8 @@
+num1=1
+num2=2
+num3=67
+num3=num1
+num1=num2
+num2=num3
+console.log(`num1= ${num2}`);
+console.log(`num2= ${num3}`);
